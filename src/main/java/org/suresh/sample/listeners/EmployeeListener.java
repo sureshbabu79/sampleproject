91484c4d17286b21b59a17183148b7df6dd29380
Suresh <sureshbabu79@live.com>
2018-02-03 19:29:09 -0800

    Added Resources and Listener classes

---
+++ src/main/java/org/suresh/sample/listeners/EmployeeListener.java
@@ -0,0 +1,14 @@
+package org.suresh.sample.listeners;
+
+//This is the Employer Listener file. Basically the project will be configured
+//in the xml file to listen to all listeners. The listener is routed to the 
+//corresponding service class. The listener will have a TRANSFORMER to convert 
+//the incoming object to the respective domain objects. The above mentioned 
+//setup is followed in BlackRock.(where as in REST we have a resource package 
+//that listens to all request.
+
+//i just put it as the matter of information.
+
+public class EmployeeListener {
+
+}
---
+++ src/main/java/org/suresh/sample/resource/EmployeeResource.java
@@ -0,0 +1,7 @@
+package org.suresh.sample.resource;
+
+//This class file is defined for the rest related activity
+
+public class EmployeeResource {
+
+}
