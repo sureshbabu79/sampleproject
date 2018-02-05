c6c42d11a71c98a8360b518324870723327e4eb9
Suresh <sureshbabu79@live.com>
2018-02-05 00:32:40 -0800

    Main class

---
+++ src/main/java/org/suresh/server/SampleProjectMain.java
@@ -0,0 +1,9 @@
+package org.suresh.server;
+
+//starting point of the server
+public class SampleProjectMain {
+
+	public static void main(String[] args) {
+		System.out.println("Main Program called...");
+	}
+}
